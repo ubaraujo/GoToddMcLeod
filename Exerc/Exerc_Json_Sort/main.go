9f800af8b12fc272ad04a1723852ddb8f443b089
Tom Araújo <ubaraujo@protonmail.com>
2025-09-13 01:46:37 -0300

    done with json and sort exercises

--- Exerc/Exerc_Json_Sort/main.go
+++ Exerc/Exerc_Json_Sort/main.go
@@ -37,7 +37,7 @@ func main() {
 
 	fmt.Println(users)
 
-	// your code goes here
+	// Exerc #1
 
 	byteSlice, err := json.Marshal(users)
 	if err != nil {
@@ -45,7 +45,7 @@ func main() {
 	}
 	fmt.Println(string(byteSlice))
 
-	// UNMARSHALLING
+	// Exerc #2
 
 	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
 	fmt.Println(s)
